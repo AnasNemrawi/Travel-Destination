b83e9be487a4931994b66536fd09be0061999f86
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-05-02 07:34:42 +0000

    add a react project

--- src/component/Tours.js
+++ src/component/Tours.js
@@ -1,14 +1,17 @@
 
-
 function Tours(props) {
     let { data } = props;
     console.log(props);
     return (
         <div>
-            {data.map(item => (
+            {data.map((item) => (
                 <div key={item.id}>
                     <p>{item.name}</p>
-                    <img src={item.image} alt={item.name} />
+                    <img
+                        src={item.image}
+                        alt={item.name}
+                        style={{ width: "50%", height: "auto" }}
+                    />
                     <hr />
                 </div>
             ))}
